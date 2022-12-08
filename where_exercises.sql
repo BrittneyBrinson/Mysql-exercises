47ef370d7d87b9c038ed794b35f6c7ab1dea7caa
Brittney Brinson <brittneyc95@outlook.com>
2022-12-08 10:59:06 -0500

    Finished MySQL WHERE exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -30,3 +30,24 @@ WHERE gender = 'M'
 AND first_name = 'Irena'
 OR first_name = 'Vidya'
 Or first_name = 'Maya';
+SELECT gender, first_name
+FROM employees
+WHERE gender = 'M%'
+AND first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name ='Maya';
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND (last_name NOT LIKE '%qu%');
