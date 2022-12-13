8d048f3781ccdbedbbd68e967406c83a6feb61bc
Brittney Brinson <brittneyc95@outlook.com>
2022-12-12 20:49:08 -0500

    Finished Mysql Aliases

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -8,3 +8,7 @@ LIMIT 10;
 SELECT birth_date AS DOB, CONCAT(emp_no, '-' ,first_name,' ', last_name) AS full_name
 FROM employees
 LIMIT 10;
+SELECT CONCAT(emp_no,'-',last_name,', ',first_name) AS full_name
+, birth_date AS DOB
+FROM employees
+LIMIT 10;
